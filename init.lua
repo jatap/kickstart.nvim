13600a523dedd7e7853f2acd957747d6052a4a33
Julio Antúnez Tarín (Software Engineer Manager) <julio.antunez.tarin@gmail.com>
2026-01-12 01:15:49 +0100

    feat: switch colorscheme from modus to koda
    Add koda.nvim colorscheme plugin and set it as the active theme.
    Modus theme configuration remains available but commented out
    for easy rollback if needed.

--- init.lua
+++ init.lua
@@ -519,7 +519,16 @@ require('lazy').setup({
         },
       }
 
-      vim.cmd.colorscheme 'modus'
+      -- vim.cmd.colorscheme 'modus'
+    end,
+  },
+  {
+    'oskarnurm/koda.nvim',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- require("koda").setup({ transparent = true })
+      vim.cmd 'colorscheme koda'
     end,
   },
 
