9e41fc2ef1490a60bb87aefe0e3372d1c7c21795
Julio Antúnez Tarín (Software Engineer Manager) <julio.antunez.tarin@gmail.com>
2026-01-21 19:12:05 +0100

    Revert "feat: add gruvbox global theming (pre-automation)"
    This reverts commit 6e27356e3ff423d475fc1e0c66ceb0463de28ed7.

--- init.lua
+++ init.lua
@@ -519,7 +519,7 @@ require('lazy').setup({
         },
       }
 
-      -- vim.cmd.colorscheme 'modus'
+      vim.cmd.colorscheme 'modus'
     end,
   },
   {
@@ -529,38 +529,6 @@ require('lazy').setup({
     config = function()
       -- require("koda").setup({ transparent = true })
       -- vim.cmd 'colorscheme koda'
-    end,
-  },
-  {
-    'ellisonleao/gruvbox.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Default options:
-      require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      })
-      vim.cmd("colorscheme gruvbox")
     end,
   },
 
